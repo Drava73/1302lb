3f6d57ab06c6ab4e7a7f6a5071de27f13cd77103
My Name <kiykobogdan@gmail.com>
2023-02-26 22:31:29 +0200

    Add project files.

---
+++ 1302lb/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1302lb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Animal[] zoo = new Animal[2];
+            //zoo[0] = new Tiger();
+            //zoo[1] = new Crocodile();
+            //foreach(var i in zoo){
+            //    i.Print();
+            //    Console.WriteLine();
+            //}
+
+
+
+            Figure[] figures = new Figure[4];
+            figures[0] = new Circle(2);
+            figures[1] = new Right_tirangle(2, 2);
+            figures[2] = new Trapez(2, 4, 2);
+            figures[3] = new Rectangle(2, 2);
+            foreach (var f in figures)
+            {
+                Console.WriteLine(f.Area());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
